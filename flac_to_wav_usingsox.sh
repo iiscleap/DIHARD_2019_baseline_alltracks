2f3438a0df856f16af92ec1af1e23ccd6a43f189
Neville Ryant <nryant@gmail.com>
2019-05-08 15:29:57 -0400

    Add helper script for performing speech denoising.

--- flac_to_wav_usingsox.sh
+++ flac_to_wav_usingsox.sh
@@ -2,6 +2,9 @@ listofdevfiles=$1
 pathtoflacfiles=$2
 outputdir=$3
 
+if [[ ! -d $outputdir ]]; then
+    mkdir $outputdir
+fi
 cat $listofdevfiles | while read i
 do
     sox ${pathtoflacfiles}/${i}.flac -t wavpcm ${outputdir}/${i}.wav
