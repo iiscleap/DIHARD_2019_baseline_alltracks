eff5f37c173e76535599d050f6c4edd44eb683bd
Neville Ryant <nryant@gmail.com>
2019-05-08 15:11:34 -0400

    Eliminate local copies of VAD code.
    Switch to scripts that wrap the official tool at https://github.com/staplesinLA/denoising_DIHARD18.git.

--- run_vad.sh
+++ run_vad.sh
@@ -1,11 +1,19 @@
-	
 #!/bin/bash
-
-
-output_dir=$1
-
-
-
-python main_get_vad.py --wav_dir $output_dir --mode 3 --hoplength 30 || exit 1
-
-exit 0
+DIR=`dirname $0`
+BINDIR=$DIR/denoising_DIHARD18
+PYTHON=python
+NJOBS=1 # Number of parallel processes to use during VAD. Higher number yield
+        # faster runtimes.
+wav_dir=$1
+output_dir=$2
+if [[ ! -d $BINDIR ]]; then
+    echo "ERROR: Could not locate the main_get_vad.py script. Please change to "
+    echo "the directory containing this script and run the following command "
+    echo ""
+    echo "    git lfs clone https://github.com/staplesinLA/denoising_DIHARD18.git"
+    exit 1
+fi
+$PYTHON $BINDIR/main_get_vad.py \
+	--mode 3 --hoplength 30 --fs_vad 16000 \
+	--speech_label speech --output_ext .lab --n_jobs $NJOBS \
+	--wav_dir $wav_dir --output_dir $output_dir
